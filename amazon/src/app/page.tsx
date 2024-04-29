f581489565ca25fd72bbca9859fa3a51bd6e9c5f
Norton Choy <nomvus848@gmail.com>
2024-04-29 14:05:54 -0700

    added group name

--- amazon/src/app/page.tsx
+++ amazon/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <h2>Group 2</h2>
       <p>Kevin Schultz</p>
       <p> Alan Tan </p>
       <p>Simon Kwong</p>
