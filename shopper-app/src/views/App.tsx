1b87d639c5c6eccd2e8240af9ae9b4de9e53dd51
Norton Choy <nomvus848@gmail.com>
2024-05-02 21:29:22 -0700

    GL-#20 added translation functionality to hello world page

--- shopper-app/src/views/App.tsx
+++ shopper-app/src/views/App.tsx
@@ -1,9 +1,25 @@
 import { Fragment } from "react";
 import Home from "./Home";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+
+// temporary button to swap languages
+const Switcher = () => {
+    const router = useRouter()
+    const changeTo = router.locale === 'en' ? 'zh' : 'en'
+    const { t } = useTranslation('common')
+    return (
+      <Link href="/" locale={changeTo}>
+        <button>{t('change-locale', { changeTo })}</button>
+      </Link>
+    )
+  }
 
 export function App() {
     return (
         <Fragment>
+            <Switcher />
             <Home />
         </Fragment>
     );
