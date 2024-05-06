4e3466f1e163e629e063a7f66037217b0aaab45d
Norton Choy <nomvus848@gmail.com>
2024-05-05 17:04:06 -0700

    changed name of 'order' table to be consistent with other DBs

--- OrderService/src/order/service.ts
+++ OrderService/src/order/service.ts
@@ -5,7 +5,7 @@ import { pool } from '../db';
 export class OrderService {
   public async create(OrderInfo: OrderInfo): Promise<OrderReponse|undefined> {
     let insert = 
-      `INSERT INTO order_table (data, vendor_id, shopper_id) VALUES 
+      `INSERT INTO order (data, vendor_id, shopper_id) VALUES 
      (jsonb_build_object('productId', $1::uuid), $2::uuid, $3::uuid) RETURNING id`;
     const query = {
       text: insert,
