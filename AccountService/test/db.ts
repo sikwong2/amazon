bb29a5644a1083552ba97ef2096ab5d6706db93d
Sunny Han <shan66@ucsc.edu>
2024-05-07 00:04:00 -0700

    avoiding lawsuit

--- AccountService/test/db.ts
+++ AccountService/test/db.ts
@@ -1,19 +1,3 @@
-/*
-#######################################################################
-#
-# Copyright (C) 2022-2024 David C. Harrison. All right reserved.
-#
-# You may not use, distribute, publish, or modify this code without
-# the express written permission of the copyright holder.
-#
-#######################################################################
-*/
-/*
-#######################################################################
-#                   DO NOT MODIFY THIS FILE
-#######################################################################
-*/
-
 import { Pool } from 'pg';
 import * as fs from 'fs';
 
