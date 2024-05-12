b8d3f313f4a039769737c074119264367ba5d38a
Kevin Schultz <ksschult@ucsc.edu>
2024-05-11 22:11:15 -0700

    removed print statements

--- AccountService/src/db.ts
+++ AccountService/src/db.ts
@@ -1,11 +1,5 @@
 import { Pool } from 'pg';
 
-console.log('POSTGRES_HOST:', process.env.POSTGRES_HOST);
-console.log('POSTGRES_PORT:', process.env.POSTGRES_PORT);
-console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
-console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
-console.log('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD);
-
 const pool = new Pool({
   host: process.env.POSTGRES_HOST || 'localhost',
   port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
