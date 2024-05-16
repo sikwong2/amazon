38272411f2530ed34b92600695091bf3f95f197d
Kevin Schultz <ksschult@ucsc.edu>
2024-05-16 12:25:08 -0700

    error if environment variables undefined in db.ts

--- AccountService/src/db.ts
+++ AccountService/src/db.ts
@@ -1,12 +1,38 @@
 import { Pool } from 'pg';
 
-// change name
+const { POSTGRES_HOST, POSTGRES_ACCOUNT_PORT, POSTGRES_ACCOUNT_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+
+if (!POSTGRES_HOST) {
+  console.error('Error: POSTGRES_HOST is not defined in db.ts AccountService.');
+  process.exit(1);
+}
+
+if (!POSTGRES_ACCOUNT_PORT) {
+  console.error('Error: POSTGRES_ACCOUNT_PORT is not defined in db.ts AccountService.');
+  process.exit(1);
+}
+
+if (!POSTGRES_ACCOUNT_DB) {
+  console.error('Error: POSTGRES_ACCOUNT_DB is not defined in db.ts AccountService.');
+  process.exit(1);
+}
+
+if (!POSTGRES_USER) {
+  console.error('Error: POSTGRES_USER is not defined in db.ts AccountService.');
+  process.exit(1);
+}
+
+if (!POSTGRES_PASSWORD) {
+  console.error('Error: POSTGRES_PASSWORD is not defined in db.ts AccountService.');
+  process.exit(1);
+}
+
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.POSTGRES_ACCOUNT_PORT ? parseInt(process.env.POSTGRES_ACCOUNT_PORT) : 5432,
-  database: process.env.POSTGRES_ACCOUNT_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  host: POSTGRES_HOST,
+  port: Number(POSTGRES_ACCOUNT_PORT),
+  database: POSTGRES_ACCOUNT_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
 });
 
 export { pool };
