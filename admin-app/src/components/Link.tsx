4c456632879d851cbb3be31d92296c3383ee0cdf
Norton Choy <nomvus848@gmail.com>
2024-05-16 20:32:23 -0700

    added 3 different link themes

--- admin-app/src/components/Link.tsx
+++ admin-app/src/components/Link.tsx
@@ -1,23 +1,30 @@
+// source: https://mui.com/base-ui/react-input/#hook
+
 import * as React from 'react';
 import Link from '@mui/material/Link';
 import { ThemeProvider } from '@emotion/react';
+import { linkThemes } from './Theme';
 
 interface CustomLinkProps{
   href: string,
   label: string,
+  variant?: 'blue1' | 'blue2' | 'gray';
   children: React.ReactNode
 }
 
-export default function CustomLink({ href, label, children, ...rest }: CustomLinkProps) {
+export default function CustomLink({ href, label, variant='blue1', children, ...rest }: CustomLinkProps) {
+  const theme = linkThemes[variant];
   return (
-    <Link 
-      href={href} 
-      aria-label={label}
-      id={label}
-      underline="hover"
-      {...rest}
-    >
-      {children}
-    </Link>
+    <ThemeProvider theme={theme}>
+      <Link 
+        href={href} 
+        aria-label={label}
+        id={label}
+        underline="hover"
+        {...rest}
+      >
+        {children}
+      </Link>
+    </ThemeProvider>
   );
 }
