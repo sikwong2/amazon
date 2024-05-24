86507b0bc368cd88bbf201d27f9dce8114c7ee97
Norton Choy <nomvus848@gmail.com>
2024-05-24 16:05:41 -0700

    added custom price component

--- shopper-app/src/pages/product/[productId].tsx
+++ shopper-app/src/pages/product/[productId].tsx
@@ -1,3 +1,5 @@
+// source: https://mui.com/material-ui/react-grid/#responsive-values
+
 import React from 'react';
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
@@ -5,6 +7,10 @@ import { useTranslation } from "next-i18next";
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CustomCard from '@/components/Card';
+import { Typography } from '@mui/material';
+import CustomPrice from '@/components/Price';
 
 interface Product {
   name: string,
@@ -53,10 +59,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: '0px',
   textAlign: 'center',
   color: theme.palette.text.secondary,
+  boxShadow: 'none',
+  border: 'none',
+  margin: '0px',
 }));
 
 export default function Product({ product }: ProductProp) {
@@ -70,22 +78,24 @@ export default function Product({ product }: ProductProp) {
       <p> {product.rating} </p>
       <p> {product.image} </p>
       <p> {product.description} </p>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>
-            xs=8
-          </Item>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={4.5}>
+            <Item>xs=4.5</Item>
+          </Grid>
+          <Grid item xs={12} sm={5}>
+            <Item>xs=5</Item>
+          </Grid>
+          <Grid item xs={12} sm={2.5}>
+            <Item>xs=2.5</Item>
+            <Box>
+              <CustomCard type='pointy' sx={{ p:2 }}>
+                <CustomPrice value={product.price}/>
+              </CustomCard>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-      </Grid>
+      </Box>
     </>
   )
 }
