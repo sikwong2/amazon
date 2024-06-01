b5fcbdff2d69920b1a5e85271b906895b694d170
Norton Choy <nomvus848@gmail.com>
2024-06-01 02:07:19 -0700

    added persistence to login context

--- shopper-app/src/context/Login.tsx
+++ shopper-app/src/context/Login.tsx
@@ -1,5 +1,5 @@
-import { PropsWithChildren, useState, createContext, SetStateAction } from 'react';
-import { Dispatch } from 'react';
+import Loading from '@/views/Loading';
+import { PropsWithChildren, useState, createContext, useEffect } from 'react';
 
 export const LoginContext = createContext({
   userName: '',
@@ -11,9 +11,37 @@ export const LoginContext = createContext({
 });
 
 export const LoginProvider = ({ children }: PropsWithChildren<{}>) => {
+  const isBrowser = typeof window !== 'undefined';
   const [userName, setUserName] = useState('');
   const [accessToken, setAccessToken] = useState('');
   const [id, setId] = useState('');
+  const [isRendered, setIsRendered] = useState(false);
+  
+  useEffect(() => { // runs only client side during initial render
+    if (isBrowser) {
+      setUserName(sessionStorage.getItem('userName') || '');
+      setAccessToken(sessionStorage.getItem('accessToken') || '');
+      setId(sessionStorage.getItem('userId') || '');
+      setIsRendered(true);
+    }
+  }, []);
+
+  useEffect(() => { // runs after initial render
+    if (isRendered) {
+      sessionStorage.setItem('userName', userName);
+      sessionStorage.setItem('accessToken', accessToken);
+      sessionStorage.setItem('userId', id);
+    }
+  }, [userName, accessToken, id, isRendered]);
+
+  if (!isRendered) {
+    return (
+      <>
+        <Loading/>
+      </>
+    )
+  }
+  
   return (
     <LoginContext.Provider
       value={{ userName, setUserName, accessToken, setAccessToken, id, setId }}
