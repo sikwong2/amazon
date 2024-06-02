f7bc866e2ef2f2e156cb080bb9d16304005cc04a
Kevin Schultz <ksschult@ucsc.edu>
2024-06-02 12:07:41 -0700

    get the vendorid from apikey

--- vendor-api/src/orders/controller.ts
+++ vendor-api/src/orders/controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Path, Response, Route, Security } from 'tsoa';
+import { Controller, Get, Request, Response, Route, Security } from 'tsoa';
+import * as express from 'express';
 
 import { OrdersService } from './service';
 import { Order } from './index';
@@ -6,9 +7,17 @@ import { Order } from './index';
 @Route('orders')
 export class OrdersController extends Controller {
   @Security('jwt')
-  @Get('{vendorId}')
+  @Get('')
   @Response('404', 'Vendor not found')
-  public async getOrders(@Path() vendorId: string): Promise<Order[] | undefined> {
+  public async getOrders(@Request() request: express.Request): Promise<Order[] | undefined> {
+    const vendorId = request.user?.id;
+
+    if (!vendorId) {
+      console.log('Vendor API: order controller: no vendorId provided');
+      this.setStatus(404);
+      return;
+    }
+
     return new OrdersService()
       .getVendorOrders(vendorId)
       .then(async (success: Order[] | undefined): Promise<Order[] | undefined> => {
