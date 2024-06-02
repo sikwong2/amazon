c7f693c81fb46326b1ebcb034fef3fbdabade3ed
Alan <tan.alan2000@gmail.com>
2024-06-01 21:07:18 -0700

    structure done

--- shopper-app/src/views/Checkout.tsx
+++ shopper-app/src/views/Checkout.tsx
@@ -31,6 +31,8 @@ dotenv.config({ path: '../../.env' });
 import DeliveryDate from '../components/DeliveryDate'
 import Radio from '@mui/material/Radio';
 import { StripeProduct } from '@/graphql/stripe/schema';
+import Divider from '@mui/material/Divider'
+import LockButton from '../components/LockButton'
 
 interface UserDetails {
   name: string;
@@ -150,6 +152,7 @@ export function Checkout() {
   useEffect(() => {
     (async () => {
       let total = 0;
+      let items = 0;
       const temp: any = []
       const tempStripe: StripeProduct[] = [];
       await Promise.all(
@@ -214,7 +217,13 @@ export function Checkout() {
             marginBottom: 10,
           }}
         >
-          Checkout ({cart.length} item)
+          Checkout
+        </div>
+        <div
+        style={{
+          marginRight: '250px'
+        }}>
+          <LockButton></LockButton>
         </div>
       </div>
       <Container sx={{ display: 'flex', flexDirection: 'row' }}>
@@ -234,7 +243,7 @@ export function Checkout() {
                   fontFamily: 'Amazon Ember',
                 }}
               >
-                1 Shipping address
+                1 {'    '} Shipping address
               </Typography>
             </Grid>
             <Grid item xs={6}>
@@ -258,7 +267,7 @@ export function Checkout() {
                   color: '#0f1111',
                 }}
               >
-                2 Payment method
+                2 {'    '} Payment method
               </Typography>
             </Grid>
             <Grid item xs={6}>
@@ -282,9 +291,9 @@ export function Checkout() {
               color: '#0f1111',
             }}
           >
-            3 Review items and shipping
+            3 {'    '} Review items and shipping
           </Typography>
-          <CustomCard sx={{ display: 'block', minHeight: '100%' }}>
+          <CustomCard sx={{ display: 'block', minHeight: '100%', width: '95%', marginLeft: '35px' }}>
             <div 
               style={{
                 border: '3px dashed #ADD8E6',
@@ -304,39 +313,118 @@ export function Checkout() {
                 fontWeight: '700'
             }}
             > 
-              Arriving,
+              Arriving, {' '}
               <DeliveryDate offset='7'> </DeliveryDate>
             </div>
             <List>
               {cartItems}
             </List>
           </CustomCard>
-          <CustomCard sx={{marginTop: '20px'}}>
+          <CustomCard sx={{marginTop: '20px', width: '95%', marginLeft: '35px'}}>
             <Box
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
+                flexDirection: 'row',
                 alignItems: 'center',
                 color: '#0f1111',
+                flex: '1'
               }}
             >
-              {t('cart.subtotal') +
-                `(${cart.length} ${cart.length == 1 ? t('cart.item') : t('cart.items')}): 
-            $ ${subtotal}`}
-              <CustomButton
-                type="submit"
-                label="checkout"
-                variant="contained"
-                color="primary"
-                sx={{ mt: 3, mb: 2 }}
-                onClick={() => {
-                  placeOrder(stripeProducts);
-                }}
-              >
-                Place your Order
-              </CustomButton>
+              <div style={{ flex: '1' }}>
+                <CustomButton
+                  type="submit"
+                  label="checkout"
+                  variant="contained"
+                  pill
+                  color="primary"
+                  sx={{ mt: 3, mb: 2, mr: 0.1, ml: 3, flex: '1', fontSize: '13px' }}
+                  onClick={() => {
+                    placeOrder(stripeProducts);
+                  }}
+                >
+                  Place your Order
+                </CustomButton>
+              </div>
+              <div style={{display: 'flex', flexDirection: 'column', flex: '3'}}>
+                <div style={{ flex: '1', textAlign: 'left', color: '#b12704', fontWeight: '700'}}>
+                {`Order total: $${subtotal}`} 
+                </div> 
+                <div style={{flex: '1'}}>
+                  <Typography sx={{fontSize: '11px'}}> By placing your order, you agree to cse187Project's {' '} 
+                    <CustomLink
+                      label="privacy-notice"
+                      variant="blue2"
+                      href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&nodeId=468496"
+                    >
+                      privacy notice
+                    </CustomLink>
+                    {' '} and {' '}
+                    <CustomLink
+                      label="privacy-notice"
+                      variant="blue2"
+                      href="https://www.amazon.com/gp/help/customer/display.html?nodeId=GLSBYFE9MGKKQXXM"
+                    >
+                      conditions of use
+                    </CustomLink> 
+                    .
+                  </Typography>
+                </div>
+              </div>
             </Box>
           </CustomCard>
+          <CustomDivider sx={{ marginTop: 1.5 }}></CustomDivider>
+          <CustomDivider></CustomDivider>
+          <Typography sx={{marginTop: '20px', fontSize: '12px', color: 'text.secondary'}}>
+            Need help? Check our {' '}
+            <CustomLink
+              label="privacy-notice"
+              variant="blue2"
+              href="https://www.amazon.com/gp/help/customer/display.html/ref=chk_help_helpfooter_pri?nodeId=GWZRWJGNW45SAWPY&ie=UTF8&ref_=chk_help_helpfooter_pri"
+            >
+              Help pages {' '}
+            </CustomLink> 
+            or {' '}
+            <CustomLink
+              label="privacy-notice"
+              variant="blue2"
+              href="https://www.amazon.com/hz/contact-us/foresight/hubgateway"
+            >
+              contact us
+            </CustomLink> 
+          </Typography>
+          <Typography sx={{ marginTop: '5px', fontSize: '12px', color: 'text.secondary' }}>
+            For an item sold by Amazon.com: When you click the "Place your order" button, we'll send you an email message acknowledging receipt of your order. Your contract to purchase an item will not be complete until we send you an email notifying you that the item has been shipped.
+          </Typography>
+          <Typography sx={{ marginTop: '10px', fontSize: '12px', color: 'text.secondary' }}>
+            <CustomLink
+              label="privacy-notice"
+              variant="blue2"
+              href="https://www.amazon.com/gp/help/customer/display.html/ref=chk_help_statetaxfooter_pri?ie=UTF8&nodeId=202029100"
+            >
+              important information about sales tax you may owe in your state
+            </CustomLink>  
+          </Typography>
+          <Typography sx={{ marginTop: '5px', fontSize: '12px', color: 'text.secondary' }}>
+            You may return new, unopened merchandise in original within 30 days of delivery. Exceptions and restrictions apply. See
+            cse187Project's {' '}
+            <CustomLink
+              label="privacy-notice"
+              variant="blue2"
+              href="https://www.amazon.com/gp/help/customer/display.html/ref=chk_help_returnsfooter_pri?nodeId=GNW5VKFXMF72FFMR&ie=UTF8&ref_=chk_help_returnsfooter_pri"
+            >
+              Returns Policy.
+            </CustomLink>  
+          </Typography>
+          <Typography sx={{ marginTop: '10px', marginBottom: '40px', fontSize: '12px', color: 'text.secondary' }}>
+            Need to add more items to your order? Continue shopping on the {' '}
+            <CustomLink
+              label="privacy-notice"
+              variant="blue2"
+              href="https://www.amazon.com/?tag=amazusnavi-20&hvadid=675149237887&hvpos=&hvnetw=g&hvrand=2498626952770057035&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9031944&hvtargid=kwd-10573980&ref=pd_sl_7j18redljs_e&hydadcr=28883_14649097&gad_source=1"
+            >
+              ucsc-amazon.com homepage
+            </CustomLink>
+          </Typography>
         </Container>
         <Container maxWidth="sm" sx={{ flex: 1, justifyContent: 'center' }}>
           <CustomCard type="rounded" sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -365,7 +453,7 @@ export function Checkout() {
               variant="body1"
               sx={{ mt: 1, mb: 1, fontSize: '12px', textAlign: 'center', ml: '20px', mr: '20px' }}
             >
-              By placing your order, you agree to Amazon&nbsps;
+              By placing your order, you agree to cse187Project's {' '} 
               <CustomLink
                 label="privacy-notice"
                 variant="blue2"
