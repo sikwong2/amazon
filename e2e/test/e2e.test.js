61837a0c1489d38b2bffedc72b3847625e09efc0
Kevin Schultz <ksschult@ucsc.edu>
2024-06-07 13:26:03 -0700

    test-silent in ci

--- e2e/test/e2e.test.js
+++ e2e/test/e2e.test.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@jest/globals');
 
 test('Results for Apple Search', async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Navigate the page to a URL
