354794de8a1dd70f58b487ff52b0e232f08719f8
Norton Choy <nomvus848@gmail.com>
2024-07-28 21:23:47 -0700

    fixed naming scheme

--- ProductService/src/category/service.ts
+++ ProductService/src/category/service.ts
@@ -3,29 +3,20 @@ import { Category } from ".";
 
 export class CategoryService {
   public async getAll(): Promise<Category[]> {
-    const query = {
+    const select = {
       text: 'SELECT * FROM category;'
     }
-    const { rows } = await pool.query(query);
+    const { rows } = await pool.query(select);
     return rows;
   }
 
   public async getCategoriesOfProducts(productId: string): Promise<Category[]> {
-    const query = {
+    const select = {
       text: 'SELECT category_id as id, name FROM category c JOIN product_category pc ON c.id = pc.category_id WHERE pc.product_id = $1',
       values: [productId]
     }
-    const { rows } = await pool.query(query);
+    const { rows } = await pool.query(select);
     return rows;
-  }
-
-  public async create(category: string): Promise<Category | undefined> {
-    const query = {
-      text: 'INSERT INTO category (name) VALUES ($1) ON CONFLICT (name) DO NOTHING RETURNING *',
-      values: [category]
-    }
-    const { rows } = await pool.query(query);
-    return rows[0];
   }
 
   public async getCategoryData(category: string, byId=false): Promise<string|undefined> {
@@ -47,6 +38,15 @@ export class CategoryService {
     }
   }
 
+  public async create(category: string): Promise<Category | undefined> {
+    const insert = {
+      text: 'INSERT INTO category (name) VALUES ($1) ON CONFLICT (name) DO NOTHING RETURNING *',
+      values: [category]
+    }
+    const { rows } = await pool.query(insert);
+    return rows[0];
+  }
+
   public async delete(category: string, byId=false): Promise<boolean> {
     // check if category exists and get its name/id
     let deleteText;
@@ -55,11 +55,11 @@ export class CategoryService {
     } else {
       deleteText = 'SELECT id, name FROM category WHERE name = $1';
     }
-    let query = {
+    let select = {
       text: deleteText,
       values: [category]
     }
-    const categoryExists = await pool.query(query);
+    const categoryExists = await pool.query(select);
     if(categoryExists.rowCount === 0) {
       return false;
     }
@@ -67,36 +67,35 @@ export class CategoryService {
     const categoryName = categoryExists.rows[0].name;
 
     // delete from junction table
-    query = {
+    let del = {
       text: 'DELETE FROM product_category WHERE category_id = $1',
       values: [categoryId]
     }
-    await pool.query(query);
+    await pool.query(del);
 
     // get all products that have this category
-    query = {
+    select = {
       text: "SELECT id, data FROM product WHERE (data->'category') ? $1",
       values: [categoryName]
     }
-    const products = await pool.query(query);
+    const products = await pool.query(select);
 
     // remove category from products that had this category
     for (const product of products.rows) {
       const newCategories = product.data.category.filter((cat: string) => categoryName !== cat);
-      query = {
+      let update = {
         text: 'UPDATE product SET data = jsonb_set(data, \'{category}\', $1) WHERE id = $2',
         values: [JSON.stringify(newCategories), product.id]
       } 
-      await pool.query(query);
+      await pool.query(update);
     }
 
     // delete from category table
-    query = {
+    del = {
       text: 'DELETE FROM category WHERE name = $1',
       values: [categoryName]
     }
-    await pool.query(query);
-
+    await pool.query(del);
     return true;
   }
 }
