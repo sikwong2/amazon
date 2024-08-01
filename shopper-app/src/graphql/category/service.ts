c04b791d56e2d49a76ee7eee18572ffcd3cd8bba
Norton Choy <nomvus848@gmail.com>
2024-07-31 20:48:27 -0700

    added graphql queries to get categories

---
+++ shopper-app/src/graphql/category/service.ts
@@ -0,0 +1,45 @@
+import { Category } from "./schema";
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+export class CategoryService {
+  public async getAllCategories(): Promise<[Category]> {
+    try {
+      const res = await fetch(
+        `http://localhost:${process.env.PRODUCT_SERVICE_PORT}/api/v0/category`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const json = await res.json();
+      console.log("getAllCategories: ", json);
+      return json;
+    } catch (e) {
+      console.error(e);
+      throw new Error('error in CategoryService: getAllCategories');
+    }
+  }
+
+  public async getCategoriesOfProduct(productId: string): Promise<[Category]> {
+    try {
+      const res = await fetch(
+        `http://localhost:${process.env.PRODUCT_SERVICE_PORT}/api/v0/category/${productId}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const json = await res.json();
+      console.log("product categories: ", json);
+      return json;
+    } catch (e) {
+      console.error(e);
+      throw new Error('error in CategoryService: getCategoriesOfProduct');
+    }
+  }
+}
