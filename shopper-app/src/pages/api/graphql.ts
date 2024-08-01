e42f8e2507a5e093de321e219454ee41edb8e3ce
Norton Choy <nomvus848@gmail.com>
2024-07-31 20:54:37 -0700

    added CategoryResolver to schema

--- shopper-app/src/pages/api/graphql.ts
+++ shopper-app/src/pages/api/graphql.ts
@@ -8,9 +8,10 @@ import { nextAuthChecker } from '../../graphql/auth/checker';
 import { OrdersResolver } from '@/graphql/orders/resolver';
 import { MemberResolver } from '@/graphql/member/resolver';
 import { ProductResolver } from '@/graphql/product/resolver';
+import { CategoryResolver } from '@/graphql/category/resolver';
 
 const schema = buildSchemaSync({
-  resolvers: [AuthResolver, MemberResolver, ProductResolver, OrdersResolver, StripeResolver],
+  resolvers: [AuthResolver, MemberResolver, ProductResolver, OrdersResolver, StripeResolver, CategoryResolver],
   validate: true, 
   authChecker: nextAuthChecker,
 });
