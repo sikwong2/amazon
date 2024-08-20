d8a18713807321d40203d5225f23545d2fa30641
Norton Choy <nomvus848@gmail.com>
2024-08-19 22:21:14 -0700

    added category context

--- shopper-app/src/pages/_app.tsx
+++ shopper-app/src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { CartProvider } from "@/context/Cart";
 import { PageProvider } from "@/context/Page";
 import { SearchProvider } from '@/context/Search';
 import { BrowserHistoryProvider } from "@/context/BrowserHistory";
+import { CategoryProvider } from "@/context/Category";
 
 const theme = createTheme({
   typography: {
@@ -24,7 +25,9 @@ const App = ({ Component, pageProps }: AppProps) => {
           <PageProvider>
             <CartProvider>
               <BrowserHistoryProvider>
-                <Component {...pageProps} />
+                <CategoryProvider>
+                  <Component {...pageProps} />
+                </CategoryProvider>
               </BrowserHistoryProvider>
             </CartProvider>
           </PageProvider>
