df283118706023ab5ebf941c43fb4224be848358
Alan <tan.alan2000@gmail.com>
2024-09-08 12:31:06 -0700

    resolved issue for comingSoon page

--- shopper-app/src/views/ComingSoon.tsx
+++ shopper-app/src/views/ComingSoon.tsx
@@ -1,16 +1,16 @@
-import TopBar from "@/components/TopBar"
-import { Typography } from "@mui/material";
-import { useTranslation } from 'next-i18next';
-
+import { Box, Typography } from "@mui/material"; 
+import TopBar from "@/components/TopBar"; 
+import { useTranslation } from "next-i18next";
 
 export function ComingSoon() {
-  const { t } = useTranslation(); // No need to specify 'common' if accessing a root-level key
+  const { t } = useTranslation();
+
   return (
-    <div style={{ display: 'flex', justifyContent: "center", flexDirection: 'column' }}>
+    <Box style={{ display: 'flex', justifyContent: "center", flexDirection: 'column' }}>
       <TopBar />
-      <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h4">{t('coming-soon')}</Typography> {/* Adjust the variant as needed */}
-      </div>
-    </div>
+      <Box style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography variant="h4">{t('coming-soon')}</Typography> 
+      </Box>
+    </Box>
   )
 }
